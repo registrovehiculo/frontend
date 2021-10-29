4998b39ce2c643d9801f3e77b288206634fc93bc
Edward Castle <sir.edwardcastle@gmail.com>
2021-10-28 22:40:47 -0400

    try to fixed cookies problems once again

--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,37 @@
 
 <script>
 // Components
+import { mapGetters, mapMutations } from 'vuex'
 import HomePageTransporte from '~/components/HomePageTransporte'
 import Homepage from '~/components/Homepage'
 export default {
   components: { Homepage, HomePageTransporte },
-  auth: false
+  auth: false,
+  beforeMount() {
+    if (this.$auth.loggedIn) {
+      if (!this.getUser().username) {
+        this.logout()
+      }
+    }
+  },
+  methods: {
+    async logout() {
+      this.$cookies.remove('auth.user')
+      await this.$auth.logout().then(() => {
+        this.cleanAuth()
+      })
+      // window.location.href = '/'
+      this.$router.replace('/').catch(() => {})
+    },
+    ...mapGetters({
+      getUser: 'getUser',
+      getUsername: 'getUsername'
+    }),
+    ...mapMutations({
+      loadAuth: 'loadFromCookie',
+      cleanAuth: 'cleanAuth'
+    })
+  }
 }
 </script>
 <style lang="stylus" scoped></style>
